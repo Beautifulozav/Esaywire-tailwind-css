21d607ded1068c43ec3870673ac8b56665957f1f
praizoh <oremei.akande@gmail.com>
2022-08-05 19:35:16 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'blue':"#2184A4",
+        'white':"#FFFFFF",
+        'gray':"#8A8585",
+        'lightblue':"#36A1C3",
+        'lightblack':"#494747",
+        'card':"#7A7373",
+        'lightgray':"#F9F9F9",
+        'darkgray':"#F5F5F5",
+        'green':"#0D8C49",
+        'process':"#AB4B15",
+        'failed':"#6E7571",
+        'img':"#2FFEFE",
+        'ai':"#055D79",
+        'add':"#F0F0F0",
+        'home':"#055D79v",
+        'ho':"#055D79",
+      }
+    },
+  },
+  plugins: [],
+}
